19b3b4f1240456404503d5e757adf0f8ba2a5d50
Isegrimer <netrosferus@gmail.com>
2016-12-03 11:46:13 -0600

    Scope

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-var a = 'Hello world';
-console.log(a);
-
-b();
+console.log(myVar);
 
 function b() {
-  console.log("Called b");
+  console.log("funtion b started "+myVar);
+  var myVar;
+  console.log("inside b"+myVar);
 }
+
+function a() {
+  console.log("funtion a started"+myVar);
+  var myVar= 2;
+  console.log("inside a: "+myVar);
+  b();
+  console.log("inside a after called b"+ myVar);
+}
+
+var myVar = 1;
+console.log("before call a "+myVar);
+a();
+console.log("after call a "+myVar);
